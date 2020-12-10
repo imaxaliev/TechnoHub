32db1079317ddffa93d6284959403f968cb0200c
Maksim <anxious-mind>
2020-12-11 01:49:22 +0300

    rename parent migration.

--- store/migrations/0017_auto_20200913_2251.py
+++ store/migrations/0017_auto_20200913_2251.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('store', '0015_auto_20200913_1753.py'),
+        ('store', '0015_auto_20200913_1753'),
     ]
 
     operations = [
